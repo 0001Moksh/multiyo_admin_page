8a312dc635a8b9acf8a8f30c355a092351ae0f27
MOKSH BHARDWAJ <164907693+0001Moksh@users.noreply.github.com>
2026-02-03 13:14:36 +0530

    Remove problematic public route from vercel.json

--- next.config.js
+++ next.config.js
@@ -4,19 +4,6 @@ const nextConfig = {
   images: {
     domains: ['cdn.shopify.com'],
   },
-  headers: async () => {
-    return [
-      {
-        source: '/public/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
